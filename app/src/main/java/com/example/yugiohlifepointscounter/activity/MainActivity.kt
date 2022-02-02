5f183f1bc7e4711c13692daada078fc6fd33d3cc
Sebastian Correa <correa.sebas6@gmail.com>
2022-02-02 09:02:15 -0500

    updated ui & added timer

---
+++ app/src/main/java/com/example/yugiohlifepointscounter/activity/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.yugiohlifepointscounter.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.TextView
+import com.example.yugiohlifepointscounter.R
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var START_TIME_IN_MILLIS: Long = 2400000
+    private var timeLeftInMillis = START_TIME_IN_MILLIS
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.timer_textview).setOnClickListener {
+            startTimeCounter()
+        }
+    }
+
+    private fun startTimeCounter() {
+        val countTime: TextView = findViewById(R.id.timer_textview)
+
+        object : CountDownTimer(START_TIME_IN_MILLIS, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMillis = millisUntilFinished
+                updateCountDownText()
+                countTime.isClickable = false
+            }
+
+            override fun onFinish() {
+                countTime.text = "Done!"
+                countTime.isClickable = true
+            }
+        }.start()
+    }
+
+    private fun updateCountDownText() {
+        val minutes = (timeLeftInMillis / 1000) / 60
+        val seconds = (timeLeftInMillis / 1000) % 60
+
+        val timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+
+        findViewById<TextView>(R.id.timer_textview).text = timeLeftFormatted
+    }
+}
